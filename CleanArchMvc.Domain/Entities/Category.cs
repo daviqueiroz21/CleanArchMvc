3bee2697c27609a3d6bd6650f3fd0a02c116affe
Davi <davisilvaqueiroz@outlook.com>
2023-12-02 00:54:31 -0300

    Criando as entidades do modelo domínio Anemica para conhecimento

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public ICollection<Product> Products{ get; set;}
+    }
+}
---
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Entities
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Image {  get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
