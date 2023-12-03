c8136e1223283ef3e80c7ec1f578cd9590832f8f
Davi <davisilvaqueiroz@outlook.com>
2023-12-03 17:29:58 -0300

    Configurando as entidades usando a Fluent API

---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+                new Category(1, "Material Escolar"),
+                new Category(2, "Eletrônicos"),
+                new Category(3, "Acessórios"));
+        }
+    }
+}
---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using CleanArchMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+
+            builder.Property(x => x.Description).HasMaxLength(200).IsRequired();
+
+            builder.Property(x => x.Price).HasPrecision(10, 2);
+
+            builder.HasOne(x => x.Category).WithMany(x => x.Products).HasForeignKey(e => e.CategoryId);
+        }
+    }
+}
