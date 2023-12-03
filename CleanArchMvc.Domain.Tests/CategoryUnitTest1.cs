f6a23846f9049aa1268b70b83b7ffa348b011178
Davi <davisilvaqueiroz@outlook.com>
2023-12-03 15:56:36 -0300

    Testes de unidade para a entidade Product

---
+++ CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
@@ -0,0 +1,53 @@
+using CleanArchMvc.Domain.Entities;
+using FluentAssertions;
+
+namespace CleanArchMvc.Domain.Tests
+{
+    public class CategoryUnitTest1
+    {
+        [Fact(DisplayName = "Create Category Object With Valid State")]
+        public void CreateProduct_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Category(1, "Category Name");
+            action.Should()
+                .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create Category Object With Invalid State")]
+        public void CreateProduct_WithValidParameters_ResultObjectInvalidState()
+        {
+            Action action = () => new Category(-1, "Category Name");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value");
+        }
+
+
+        [Fact(DisplayName = "Create Category with Short name Value")]
+        public void CreateProduct_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Category(1, "Ca");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minimum 3 characters");
+        }
+
+
+        [Fact(DisplayName = "Create Category with Missing name Value")]
+        public void CreateProduct_MissingNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Category(1, "");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name.Name is required");
+        }
+        [Fact(DisplayName = "Create Category with Null name Value")]
+        public void CreateProduct_WithNullNameValue_ValidObject()
+        {
+            Action action = () => new Category(1, null);
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name.Name is required");
+        }
+    }
+}
